e06d8eae5a753a9987eec6a71f59ce7a2d977b01
Oleksandr Tril <tril.oleksandr@gmail.com>
2020-08-10 19:49:32 +0300

    pushing  hw 7

---
+++ build/precache-manifest.a2d3114c3b11cfa44d17a6915f6ade02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b4a066da75deaf5539d11264197b18c",
+    "url": "/goit-react-hw-07-phonebook/index.html"
+  },
+  {
+    "revision": "1f8109c6947de31b59f5",
+    "url": "/goit-react-hw-07-phonebook/static/css/main.429fa57d.chunk.css"
+  },
+  {
+    "revision": "f891063e5dccf18af90c",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.000ca4f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.000ca4f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f8109c6947de31b59f5",
+    "url": "/goit-react-hw-07-phonebook/static/js/main.86dd0dcf.chunk.js"
+  },
+  {
+    "revision": "219ee63eab378a07bfb5",
+    "url": "/goit-react-hw-07-phonebook/static/js/runtime-main.e14b549b.js"
+  }
+]);
